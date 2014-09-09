64ad044c2ed52079f0e2da54549e3856ab259c49
Mido <mohamed.ali.ahmed92@gmail.com>
2014-09-10 01:31:43 +0200

    build for heroku

--- dist/scripts/scripts.b207ad1d.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("groupeerApp",["ngAnimate","ngResource"]),angular.module("groupeerApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
