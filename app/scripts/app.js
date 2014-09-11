15487118cb47ec8941d68c276cec39a3e0b15f66
Mido <mohamed.ali.ahmed92@gmail.com>
2014-09-11 03:48:29 +0200

    search fields and css stuffs <3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,17 +16,38 @@ var groupy = angular
     'ui.router'
   ]);
 
-groupy.controller('homeController', ["$scope", function($scope){
-  // $scope.projects = [
-  //   'Mido Design',
-  //   'Koala Design',
-  //   'Many other stuff',
-  //   'cooool stuff'
-  // ];
-  // $scope.selected = undefined;
+groupy.controller('homeController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+  var activeCategories = [];
+
+  $http.get('data/projects.json')
+    .success(function(data) {
+      $scope.projects = data.projects;//.concat(data.services)
+    });
+
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  };
+
+  $scope.categoryFilter = function(category) {
+    var ind = activeCategories.indexOf(category);
+    if (ind === -1) {
+      activeCategories.push(category);
+    }
+    else {
+      activeCategories.splice(ind, 1);
+    }
+    $http.get('data/projects.json')
+      .success(function(data) {
+        $scope.projects.length = 0;
+        for (var i = activeCategories.length - 1; i >= 0; i--) {
+          // debugger;
+          $scope.projects = $scope.projects.concat(data[activeCategories[i]]);
+        }
+    });
+  };
 }]);
 
-groupy.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+groupy.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
@@ -44,3 +65,11 @@ groupy.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, 
       templateUrl: 'views/sign-up.html'
     });
 }]);
+
+groupy.directive('hoverSidebar', function(){
+  return {
+    restrict: 'AE',
+    templateUrl: 'views/hoverSidebar.html',
+    replace: true,
+  };
+});
